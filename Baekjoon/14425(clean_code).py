6cb0eb4475e814665d489e15b87895057a880731
Seonwook, Ko <92377162+seonwook97@users.noreply.github.com>
2021-10-23 19:50:21 +0900

    Delete 14425(clean_code).py

--- Baekjoon/14425(clean_code).py
+++
@@ -1,9 +0,0 @@
-import sys
-n,m = map(int, input().split())
-count = 0
-strings = {sys.stdin.readline() for i in range(n)}
-for j in range(m):
-    a = sys.stdin.readline()
-    if a in strings:
-        count += 1
-print(count)
