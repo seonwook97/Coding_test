b2439175427c88fc6c06d6dc1e6e857883701bd6
Seonwook, Ko <92377162+seonwook97@users.noreply.github.com>
2021-10-30 19:20:08 +0900

    Update 10845.py

--- Baekjoon/10845.py
+++ Baekjoon/10845.py
@@ -12,31 +12,40 @@ for i in range(cmd_num):
     if cmd[0] == 'push':
         queue.append(cmd[1])
 
+        
     elif cmd[0] == 'pop':
+        
         if len(queue) != 0:
             print(queue.pop(0))
 
         else:
             print(-1)
 
+            
     elif cmd[0] == 'size':
         print(len(queue))
 
+        
     elif cmd[0] == 'empty':
+        
         if len(queue) == 0:
             print(1)
 
         else:
             print(0)
 
+            
     elif cmd[0] == 'front':
+        
         if len(queue) == 0:
             print(-1)
 
         else:
             print(queue[0])
 
+            
     elif cmd[0] == 'back':
+        
         if len(queue) == 0:
             print(-1)
 
