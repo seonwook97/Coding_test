43859078021ed9e048dd474813b7c22caf601bca
Seonwook, Ko <92377162+seonwook97@users.noreply.github.com>
2021-10-31 16:26:38 +0900

    제로

--- Baekjoon/10773.py
+++ Baekjoon/10773.py
@@ -1,8 +1,10 @@
-# 메모리: 29980KB, 시간: 3836ms
+# 메모리: 29980KB, 시간: 108ms
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 n_list = []
-i = 0
-while i < n: # 왜 for문으로 하면 시간초과가 뜨는것인가..
+for i in range(n):
     num = int(input())
 
     if num != 0:
@@ -10,7 +12,5 @@ while i < n: # 왜 for문으로 하면 시간초과가 뜨는것인가..
 
     else:
         n_list.pop()
-
-    i += 1
 
 print(sum(n_list))
