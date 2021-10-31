4f9bb93afab8a8628916adcf2cb0f911d6cbe97b
Seonwook, Ko <92377162+seonwook97@users.noreply.github.com>
2021-10-31 16:52:51 +0900

    핸드폰 번호 가리기

--- Programmers/12948.py
+++
@@ -1,9 +0,0 @@
-def solution(a):
-    a = '*' * len(a[:-4]) + a[-4:]
-    return a
-
-# print(solution('01033334444'))
-
-
-
-
---
+++ Programmers/12948_.py
@@ -0,0 +1,9 @@
+def solution(a):
+    a = '*' * len(a[:-4]) + a[-4:]
+    return a
+
+# print(solution('01033334444'))
+
+
+
+
