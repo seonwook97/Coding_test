2a0b13c554896470f8e39c70596d13222336ec4f
Seonwook, Ko <92377162+seonwook97@users.noreply.github.com>
2021-11-01 22:28:38 +0900

    에디터

--- Baekjoon/Linked list (연결 리스트)/1406.py
+++ Baekjoon/Linked list (연결 리스트)/1406.py
@@ -1,1 +1,32 @@
-..
+import sys
+input = sys.stdin.readline
+
+s = [input().rstrip()]
+cmd_num = int(input())
+edit_s = []
+
+for i in range(cmd_num):
+    cmd = input().split()
+
+    if cmd[0] == 'L' and s:
+        edit_s.append(s.pop())
+
+    elif cmd[0] == 'D' and edit_s:
+        s.append(edit_s.pop())
+
+    elif cmd[0] == 'B' and s:
+        s.pop()
+
+    elif cmd[0] == 'P':
+        s.append(cmd[1])
+
+print(''.join(s + list(reversed(edit_s))))
+
+
+
+
+
+
+
+
+
