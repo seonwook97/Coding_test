3f2794e06cc6a868f7afbe29137c19e52920bfcc
Seonwook, Ko <92377162+seonwook97@users.noreply.github.com>
2021-11-02 00:26:06 +0900

    에디터

--- Baekjoon/Linked list (연결 리스트)/1406.py
+++ Baekjoon/Linked list (연결 리스트)/1406.py
@@ -1,10 +1,10 @@
+# 메모리: 39620KB, 시간: 348ms
 import sys
 input = sys.stdin.readline
 
-s = [input().rstrip()]
+s = list(input().rstrip())
 cmd_num = int(input())
 edit_s = []
-
 for i in range(cmd_num):
     cmd = input().split()
 
@@ -20,7 +20,13 @@ for i in range(cmd_num):
     elif cmd[0] == 'P':
         s.append(cmd[1])
 
-print(''.join(s + list(reversed(edit_s))))
+print(''.join(s + edit_s[::-1]))
+
+# s와 edit_s를 각각 다른 두개의 스택
+# 커서는 s의 top
+# 커서를 왼쪽으로 움직이면 s의 값을 pop하여 edit_s로 append
+# 커서를 오른쪽으로 움직이면 edit_s의 값을 pop하여 s로 append
+
 
 
 
