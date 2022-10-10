eb8a033c94c4f38a6fbc5c7cf10f25073e3f2d9f
seonwook97 <rhtjsdnr9999@gmail.com>
2022-10-10 20:19:44 +0900

    해시 - 위장

---
+++ Programmers/42578.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+def solution(clothes):
+    answer = 1
+    clothes_dict = defaultdict(list)
+    for k, v in clothes:
+        clothes_dict[v].append(k)
+
+    for k in clothes_dict.keys():
+        answer = answer * (len(clothes_dict[k]) + 1) # 안 입은 경우 + 1
+
+    answer = answer - 1 # 아무것도 안 입는 경우 제외
+    return answer
+
+
+
+
+
