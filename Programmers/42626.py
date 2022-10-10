f3b76a597062e743ef73e5c9604fb00b157f4fad
seonwook97 <rhtjsdnr9999@gmail.com>
2022-10-10 21:16:40 +0900

    힙 - 더 맵게

---
+++ Programmers/42626.py
@@ -0,0 +1,16 @@
+import heapq
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    answer = 0
+    while scoville[0] < K:
+        try:
+            min = heapq.heappop(scoville)
+            heapq.heappush(scoville, min + 2 * heapq.heappop(scoville))
+            answer += 1
+
+        except IndexError:
+            return -1
+    return answer
+
+
+
