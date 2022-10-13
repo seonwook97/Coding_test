88c29e577d1ce6bb0a993d43ed70b089d9fa463a
seonwook97 <rhtjsdnr9999@gmail.com>
2022-10-13 22:27:02 +0900

    2019 카카오 개발자 겨울 인턴십 - 튜플

---
+++ Programmers/64065.py
@@ -0,0 +1,12 @@
+def solution(s):
+    s = s[2:-2].split('},{')
+    s.sort(key=len)
+    answer = []
+    for item in s:
+        num = item.split(',')
+        for item2 in num:
+            if int(item2) not in answer:
+                answer.append(int(item2))
+    return answer
+
+
