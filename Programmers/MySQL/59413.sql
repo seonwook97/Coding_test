e20e638854d5ca89cba4abd525ad7d0446b11415
seonwook97 <rhtjsdnr9999@gmail.com>
2023-03-07 23:43:18 +0900

    입양 시각 구하기(2)

---
+++ Programmers/MySQL/59413.sql
@@ -0,0 +1,7 @@
+set @hour := -1;
+
+select (@hour := @hour + 1) as hour, 
+(select count(*) from animal_outs where hour(datetime) = @hour) as count
+from animal_outs
+where @hour < 23 
+order by hour
