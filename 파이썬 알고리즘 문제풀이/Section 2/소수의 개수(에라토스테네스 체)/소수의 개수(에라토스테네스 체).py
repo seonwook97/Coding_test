b20b87ff25760c2f725edd929108e01fae095059
seonwook97 <rhtjsdnr9999@gmail.com>
2023-03-15 01:43:31 +0900

    study

--- 파이썬 알고리즘 문제풀이/Section 2/소수의 개수(에라토스테네스 체)/소수의 개수(에라토스테네스 체).py
+++ 파이썬 알고리즘 문제풀이/Section 2/소수의 개수(에라토스테네스 체)/소수의 개수(에라토스테네스 체).py
@@ -1,22 +1,31 @@
 import sys
 # sys.stdin = open('in2.txt', 'rt')
 n = int(input())
+check = [0] * (n + 1)
+cnt = 0
+for i in range(2, n + 1):
+    if check[i] == 0:
+        cnt += 1
+        for j in range(i, n + 1, i):
+            check[j] = 1
 
-def count_primes(n):
-    num_list = [True] * (n + 1)
-    num_list[0], num_list[1] = False, False
+print(cnt)
 
-    # 에라토스테네스의 체 알고리즘
-    for i in range(2, int(n ** 0.5) + 1):
-        if num_list[i]:
-            for j in range(i * i, n + 1, i):
-                num_list[j] = False
+# def count_primes(n):
+#     num_list = [True] * (n + 1)
+#     num_list[0], num_list[1] = False, False
 
-    cnt = 0
-    for num in num_list:
-        if num:
-            cnt += 1
+#     # 에라토스테네스의 체 알고리즘
+#     for i in range(2, int(n ** 0.5) + 1):
+#         if num_list[i]:
+#             for j in range(i * i, n + 1, i):
+#                 num_list[j] = False
+
+#     cnt = 0
+#     for num in num_list:
+#         if num:
+#             cnt += 1
     
-    return cnt
+#     return cnt
 
-print(count_primes(n))
+# print(count_primes(n))
