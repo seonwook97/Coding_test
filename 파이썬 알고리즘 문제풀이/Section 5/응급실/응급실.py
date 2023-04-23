8f4f0d2e2a92304ce3e61a95e8a6afa678e0ab90
seonwook97 <rhtjsdnr9999@gmail.com>
2023-04-23 23:54:09 +0900

    study

---
+++ 파이썬 알고리즘 문제풀이/Section 5/응급실/응급실.py
@@ -0,0 +1,27 @@
+import math
+import time
+import sys
+from collections import deque
+# sys.stdin = open('in1.txt', 'rt')
+# 실행시간 체크
+# start = time.time()
+
+# code
+n, m = map(int, input().split())
+Q = [(pos, val) for pos, val in enumerate(list(map(int, input().split())))]
+Q = deque(Q)
+cnt = 0
+
+while True:
+    cur=Q.popleft()
+    if any(cur[1] < x[1] for x in Q):
+        Q.append(cur)
+
+    else:
+        cnt += 1
+        if cur[0] == m:
+            print(cnt)
+            break
+
+# end = time.time()
+# print(f"{end - start:.5f} sec")
