d07b78065d1c7fac652ea6a2aa2de6c59b120f88
seonwook97 <rhtjsdnr9999@gmail.com>
2023-05-04 00:44:24 +0900

    study

---
+++ 파이썬 알고리즘 문제풀이/Section 5/최대힙/최대힙.py
@@ -0,0 +1,27 @@
+import math
+import time
+import sys
+import heapq as hq
+# sys.stdin = open('in1.txt', 'rt')
+# 실행시간 체크
+# start = time.time()
+
+# code
+a = []
+while True:
+    n = int(input())
+    if n == -1:
+        break
+
+    if n == 0:
+        if len(a) == 0:
+            print(-1)
+
+        else:
+            print(-hq.heappop(a))
+
+    else:
+        hq.heappush(a, -n)
+
+# end = time.time()
+# print(f"{end - start:.5f} sec")
