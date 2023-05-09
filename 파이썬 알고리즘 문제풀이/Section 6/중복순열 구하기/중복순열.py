d8d06bb23602d656f5b90368a5a73246bcd2b3a9
seonwook97 <rhtjsdnr9999@gmail.com>
2023-05-09 23:58:44 +0900

    study

---
+++ 파이썬 알고리즘 문제풀이/Section 6/중복순열 구하기/중복순열.py
@@ -0,0 +1,30 @@
+import math
+import time
+import sys
+# sys.stdin = open('in1.txt', 'rt')
+# 실행시간 체크
+# start = time.time()
+
+# code
+def DFS(L):
+    global cnt
+    if L == m:
+        for i in range(m):
+            print(res[i], end=' ')
+        print()
+        cnt += 1
+
+    else:
+        for i in range(1, n + 1):
+            res[L] = i
+            DFS(L + 1)
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    res = [0] * n
+    cnt = 0
+    DFS(0)
+    print(cnt)
+
+# end = time.time()
+# print(f"{end - start:.5f} sec")
