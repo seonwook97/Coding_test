c11c81c469ebf80e55cb2b7fb0ce363f45874625
seonwook97 <rhtjsdnr9999@gmail.com>
2024-01-05 00:01:07 +0900

    오프라인/온라인 판매 데이터 통합하기

--- Programmers/MySQL/131537.sql
+++ Programmers/MySQL/131537.sql
@@ -1,1 +1,28 @@
-code 
+with cte as 
+(
+    select
+        date_format(sales_date, '%Y-%m-%d') as sales_date,
+        product_id,
+        user_id,
+        sales_amount
+    from online_sale
+    where 1=1
+    and year(sales_date) = 2022
+    and month(sales_date) = 3
+    union
+    select
+        date_format(sales_date, '%Y-%m-%d') as sales_date,
+        product_id,
+        null as user_id,
+        sales_amount
+    from offline_sale
+    where 1=1
+    and year(sales_date) = 2022
+    and month(sales_date) = 3
+)
+select
+    *
+from
+    cte
+order by
+    1, 2, 3
