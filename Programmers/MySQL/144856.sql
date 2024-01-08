fb365b2fd4af5120d356dd74ce7393704dcfbb3b
seonwook97 <rhtjsdnr9999@gmail.com>
2024-01-08 22:53:39 +0900

    저자 별 카테고리 별 매출액 집계하기

--- Programmers/MySQL/144856.sql
+++ Programmers/MySQL/144856.sql
@@ -8,15 +8,15 @@ from
 join author b on a.author_id = b.author_id
 join
 (
-select
-    book_id,
-    sum(sales) sales
-from
-    book_sales
-where 1=1
-and date_format(sales_date, '%Y-%m') = '2022-01'
-group by 
-    1
+    select
+        book_id,
+        sum(sales) sales
+    from
+        book_sales
+    where 1=1
+    and date_format(sales_date, '%Y-%m') = '2022-01'
+    group by 
+        1
 ) c on a.book_id = c.book_id
 group by
     1, 2, 3
