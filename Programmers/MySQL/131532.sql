be94ae2e3f8b82fc2d3d5c83f8882b420761b238
seonwook97 <rhtjsdnr9999@gmail.com>
2024-01-10 00:01:46 +0900

    년, 월, 성별 별 상품 구매 회원 수 구하기

--- Programmers/MySQL/131532.sql
+++ Programmers/MySQL/131532.sql
@@ -1,6 +1,6 @@
 select
-    year(b.sales_date) year,
-    month(b.sales_date) month,
+    substr(b.sales_date, 1, 4) year,
+    substr(b.sales_date, 6, 2) month,
     a.gender,
     count(*) users
 from
@@ -9,9 +9,11 @@ join
 (
  select
     user_id,
-    
+    date_format(sales_date, '%Y-%m') sales_date
+ from   
     online_sale 
-
+ group by
+    1, 2
 ) b on a.user_id = b.user_id
 where 1=1
 and a.gender is not null
